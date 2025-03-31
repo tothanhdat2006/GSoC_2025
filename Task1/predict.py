9e74db6bea858d31ae7937f698bfbc40ff9b0833
ToThanhDat <tothanhdat1421@gmail.com>
2025-03-31 09:04:27 +0700

    Fix typo in task 1 & 2 and add outlier images

--- Task1/predict.py
+++ Task1/predict.py
@@ -21,11 +21,16 @@ def get_prediction(dataset, model, args, config):
         with tqdm(total=args.size, desc=f'Predicting', unit='img') as pbar:
             for i in range(args.size):
                 idx = np.random.randint(0, len(dataset))
-                image, label = dataset[idx]['images'], dataset[idx]['labels']
+                image = dataset[idx]['images']
                 image = image.unsqueeze(0).to(config['DEVICE'], dtype=torch.float32)
                 logits_pred = model(image)
                 if args.task == 'general':
-                    probs_pred = torch.softmax(logits_pred, dim=1)
+                    # logits_pred: [artist_logits, genre_logits, style_logits]
+                    print(logits_pred[0].shape)
+                    probs_pred = []
+                    probs_pred.append(torch.softmax(logits_pred[0], dim=1)) 
+                    probs_pred.append(torch.softmax(logits_pred[1], dim=1))
+                    probs_pred.append(torch.softmax(logits_pred[2], dim=1))
                     pred_artist = torch.argmax(probs_pred[0], dim=1).item()
                     pred_genre = torch.argmax(probs_pred[1], dim=1).item()
                     pred_style = torch.argmax(probs_pred[2], dim=1).item()
@@ -34,9 +39,9 @@ def get_prediction(dataset, model, args, config):
                     probs_pred = torch.softmax(logits_pred, dim=1)
                     pred = torch.argmax(probs_pred, dim=1).item()
 
-                raw_image = dataset.get_raw(idx)['images']
+                raw_image, raw_label = dataset.get_raw(idx)['images'], dataset.get_raw(idx)['labels']
                 all_images.append(raw_image)
-                y_true.append(label)
+                y_true.append(raw_label)
                 y_pred.append(pred)
                 pbar.update(1)
     
@@ -63,40 +68,52 @@ def get_outliers(dataset, model, args, config):
     outlier_probs = []
 
     with torch.no_grad():
-        for i in tqdm(total=range(len(dataset)), desc=f'Finding outliers', unit='img'):
-            idx = np.random.randint(0, len(dataset))
-            image, label = dataset[idx]['images'], dataset[i]['labels']
-            image_tensor = image.unsqueeze(0).to(config['DEVICE'], dtype=torch.float32)
-            logits_pred = model(image_tensor)
-            
-            if args.task == 'general':
-                probs_pred = torch.softmax(logits_pred, dim=1)
-                pred_artist = torch.argmax(probs_pred[0], dim=1).item()
-                pred_genre = torch.argmax(probs_pred[1], dim=1).item()
-                pred_style = torch.argmax(probs_pred[2], dim=1).item()
-                pred = [pred_artist, pred_genre, pred_style]
+        with tqdm(total=len(dataset), desc=f'Finding outliers', unit='img') as pbar:
+            for i in range(len(dataset)):
+                idx = np.random.randint(0, len(dataset))
+                image, label = dataset[idx]['images'], dataset[idx]['labels']
+                image_tensor = image.unsqueeze(0).to(config['DEVICE'], dtype=torch.float32)
+                logits_pred = model(image_tensor)
                 
-                # Check if this is an outlier (any of the three predictions has low confidence)
-                max_prob_artist = probs_pred[0][0][pred_artist].item()
-                max_prob_genre = probs_pred[1][0][pred_genre].item()
-                max_prob_style = probs_pred[2][0][pred_style].item()
+                if args.task == 'general':
+                    probs_pred = []
+                    probs_pred.append(torch.softmax(logits_pred[0], dim=1)) 
+                    probs_pred.append(torch.softmax(logits_pred[1], dim=1))
+                    probs_pred.append(torch.softmax(logits_pred[2], dim=1))
+                    pred_artist = torch.argmax(probs_pred[0], dim=1).item()
+                    pred_genre = torch.argmax(probs_pred[1], dim=1).item()
+                    pred_style = torch.argmax(probs_pred[2], dim=1).item()
+                    pred = [pred_artist, pred_genre, pred_style]
+                    
+                    # Get confidence scores for the predicted classes
+                    max_prob_artist = probs_pred[0][0][pred_artist].item()
+                    max_prob_genre = probs_pred[1][0][pred_genre].item()
+                    max_prob_style = probs_pred[2][0][pred_style].item()
+                    
+                    # Check if the correct predictions has low confidence
+                    if pred_artist == label[0] and pred_genre == label[1] and pred_style == label[2]:
+                        if max_prob_artist < args.threshold or max_prob_genre < args.threshold or max_prob_style < args.threshold:
+                            # Get raw image for display
+                            raw_image = dataset.get_raw(i)['images']
+                            outlier_images.append(raw_image)
+                            outlier_labels.append(label)
+                            outlier_preds.append(pred)
+                            outlier_probs.append([max_prob_artist, max_prob_genre, max_prob_style])
+                else:
+                    probs_pred = torch.softmax(logits_pred, dim=1)
+                    pred = torch.argmax(probs_pred, dim=1).item()
+                    max_prob = probs_pred[0][pred].item()
+
+                    # Check if the correct predictions has low confidence
+                    if pred == label:
+                        if max_prob < args.threshold:
+                            # Get raw image for display
+                            raw_image = dataset.get_raw(i)['images']
+                            outlier_images.append(raw_image)
+                            outlier_labels.append(label)
+                            outlier_preds.append(pred)
+                            outlier_probs.append(max_prob)
                 
-                if max_prob_artist < config['OUTLIER_THRESHOLD'] or max_prob_genre < config['OUTLIER_THRESHOLD'] or max_prob_style < config['OUTLIER_THRESHOLD']:
-                    raw_image = dataset.get_raw(i)['images']
-                    outlier_images.append(raw_image)
-                    outlier_labels.append(label)
-                    outlier_preds.append(pred)
-                    outlier_probs.append([max_prob_artist, max_prob_genre, max_prob_style])
-            else:
-                probs_pred = torch.softmax(logits_pred, dim=1)
-                pred = torch.argmax(probs_pred, dim=1).item()
-                max_prob = probs_pred[0][pred].item()
-                
-                if max_prob < config['OUTLIER_THRESHOLD']:
-                    raw_image = dataset.get_raw(i)['images']
-                    outlier_images.append(raw_image)
-                    outlier_labels.append(label)
-                    outlier_preds.append(pred)
-                    outlier_probs.append(max_prob)
+                pbar.update(1)
 
     return outlier_images, outlier_labels, outlier_preds, outlier_probs
